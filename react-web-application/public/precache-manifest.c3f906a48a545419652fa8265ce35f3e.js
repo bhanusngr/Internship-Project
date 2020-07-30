6ef6e7b6f04afca24360c6d1d9b59cab90d2c609
bhanusngr362 <bhanusngr@gmail.com>
2020-07-30 18:49:16 +0530

    final commit

---
+++ react-web-application/public/precache-manifest.c3f906a48a545419652fa8265ce35f3e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00123b7105e6f5de0b272b68f19e86a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5be070406967a771db62",
+    "url": "/static/css/2.1bd367ea.chunk.css"
+  },
+  {
+    "revision": "883a96e285d905e862ae",
+    "url": "/static/css/main.be3fe27f.chunk.css"
+  },
+  {
+    "revision": "5be070406967a771db62",
+    "url": "/static/js/2.389e00c0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.389e00c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "883a96e285d905e862ae",
+    "url": "/static/js/main.1cea9e03.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5cd4e8bb97dab3b9933cc3fc362c8417",
+    "url": "/static/media/headtitanicnormal.5cd4e8bb.png"
+  },
+  {
+    "revision": "54da18b97b855f640b0ceb660b866dde",
+    "url": "/static/media/output11normal.54da18b9.png"
+  },
+  {
+    "revision": "8bb9b26cbe6f6ebf5be25b80868d441c",
+    "url": "/static/media/output1normal.8bb9b26c.png"
+  },
+  {
+    "revision": "ce2b2629fb2ea3b24144d080d1cd4786",
+    "url": "/static/media/output21normal.ce2b2629.png"
+  },
+  {
+    "revision": "da1302da7e654f511677c559bc99dc24",
+    "url": "/static/media/output2normal.da1302da.png"
+  },
+  {
+    "revision": "d6a746470a13d319150f31187446d46e",
+    "url": "/static/media/plottitanicnormal.d6a74647.png"
+  },
+  {
+    "revision": "4501bb87a3527871e1ad92e3ee173c18",
+    "url": "/static/media/variablesairplanenormal.4501bb87.png"
+  },
+  {
+    "revision": "e40f9d6a903057df689ac75489017bda",
+    "url": "/static/media/variablestitanicnormal.e40f9d6a.png"
+  }
+]);
